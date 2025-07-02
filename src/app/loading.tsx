be9607b07afa1c65503f846583a3523b07363fb4
Kunal Verma <kunalverma2468@gmail.com>
2025-07-02 19:19:40 +0530

    chore: remove unused Loading component to clean up codebase

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from "@/components/ui/icons/loader";
-
-export default function Loading() {
-    return (
-        <div className="absolute w-full h-full flex place-content-center place-items-center">
-            <Loader className="size-16" />
-        </div>
-    )
-}
