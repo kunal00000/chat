5bf0ae5f3f706781f9579005626c978a6f90023e
Kunal Verma <kunalverma2468@gmail.com>
2025-07-06 18:56:00 +0530

    refactor: remove unused CHAT_ARGS configuration from chat.helpers.ts to streamline code

--- src/lib/chat.helpers.ts
+++ src/lib/chat.helpers.ts
@@ -1,24 +1,9 @@
 import { TSSEStatus } from "@/store/sse.helpers";
-import { UseChatOptions } from "@ai-sdk/react";
 import { v4 as uuid } from "uuid";
 
 export const CH = {
   isPromptBarLoading: (status: TSSEStatus) => {
     return status !== "error" && status !== "idle" && status !== "success";
-  },
-};
-
-export const CHAT_ARGS: UseChatOptions = {
-  id: "123",
-  api: "/api/chat",
-  initialInput: "",
-
-  onFinish: (...args: Parameters<NonNullable<UseChatOptions["onFinish"]>>) => {
-    console.log("ON FINISH", args);
-  },
-
-  onError: (...args: Parameters<NonNullable<UseChatOptions["onError"]>>) => {
-    console.log("ON ERROR", args);
   },
 };
 
