b4eb25edb4c1637dc3a2a1a4e4c54b37da094d58
Kunal Verma <kunalverma2468@gmail.com>
2025-07-18 00:10:13 +0530

    feat: add new chat button with tooltip in ChatPage for improved user experience

--- src/app/chat/[cid]/page.tsx
+++ src/app/chat/[cid]/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import ChatContent from "@/components/chat/chat-content";
+import TooltipWrapper from "@/components/common/tooltip-wrapper";
+import { buttonVariants } from "@/components/ui/button";
 import { PromptBarInput } from "@/components/ui/prompt-bar-input";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useChatStore } from "@/store/chat.store";
+import { PATHS } from "@/types-constants-schemas/client/chat.constants";
+import { SquarePenIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function ChatPage() {
     const chatTitle = useChatStore(s => s.chatTitle);
-
     return (
         <main className="relative flex h-[100dvh] md:rounded-tl-md md:ml-2 md:mt-2 flex-col overflow-hidden bg-background-custom">
             <header className={cn("absolute top-0 z-10 flex h-14 w-full items-center gap-2 px-4", "border-b border-border-custom/50 backdrop-blur-xl")}>
@@ -16,6 +20,15 @@ export default function ChatPage() {
                 <div className={cn("text-foreground transition-opacity duration-700 truncate", chatTitle ? "opacity-100" : "opacity-0")}>
                     {chatTitle}
                 </div>
+
+                <TooltipWrapper tooltip="New Chat" side="left">
+                    <Link
+                        href={PATHS.NEW_CHAT}
+                        className={cn(buttonVariants({ variant: "ghost", size: "icon" }), "size-7 rounded-full ml-auto")}
+                    >
+                        <SquarePenIcon size={16} />
+                    </Link>
+                </TooltipWrapper>
             </header>
 
             <ChatContent />
